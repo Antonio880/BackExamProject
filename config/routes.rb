dec21bb526be83f57a3d0253d02a7d1dab835300
Antonio880 <antoniocruzgomes880@gmail.com>
2024-07-04 00:13:06 -0300

    feat: ajustes nas rotas do rooms e exams

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
       post 'users/:user_id', to: 'rooms#add_user'
       get 'students', to: 'rooms#get_students'
     end
+    collection do
+      get 'created_by/:created_by_id', to: 'rooms#created_by'
+    end
   end
 
   resources :exams, only: [:create, :destroy] do
@@ -25,4 +28,6 @@ Rails.application.routes.draw do
       get 'room_exams/:room_id', action: :room_exams
     end
   end
+
+  get '/current_user', to: 'users#current'
 end
